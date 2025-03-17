f125eee421476816bb89f3009235fb972ea38df4
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-17 10:51:09 +0100

    Modify example #3 w. lambda and prints

--- module-3/l1-thread-creation/l1-thread-creation-3/src/main/java/sh/hahn/multithreading/module3/lecture1/example3/Main.java
+++ module-3/l1-thread-creation/l1-thread-creation-3/src/main/java/sh/hahn/multithreading/module3/lecture1/example3/Main.java
@@ -3,16 +3,17 @@ package sh.hahn.multithreading.module3.lecture1.example3;
 public class Main {
 
     public static void main(String[] args) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.printf("Current thread: %s\n", Thread.currentThread().getName());
-            }
-        };
+        System.out.printf("[1] Current thread: %s\n", Thread.currentThread().getName());
 
-        Thread myThread = new Thread(runnable);
+        Thread myThread = new Thread(
+                () -> {
+                    System.out.printf("[2] Current thread: %s\n", Thread.currentThread().getName());
+                }
+        );
         myThread.setName("MyThread");
         myThread.start();
+
+        System.out.printf("[3] Current thread: %s\n", Thread.currentThread().getName());
     }
 
 }
