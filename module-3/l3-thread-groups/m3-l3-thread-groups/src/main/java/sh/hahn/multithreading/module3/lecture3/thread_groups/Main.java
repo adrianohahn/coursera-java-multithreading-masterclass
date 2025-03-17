69db4abc0b024f6d2c8bef8e65766ea211c516c3
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-17 14:20:09 +0100

    Add thread priority example to module 3 / lecture 2

---
+++ module-3/l3-thread-groups/m3-l3-thread-groups/src/main/java/sh/hahn/multithreading/module3/lecture3/thread_groups/Main.java
@@ -0,0 +1,52 @@
+package sh.hahn.multithreading.module3.lecture3.thread_groups;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadGroup threadGroup = new ThreadGroup("MyThreadGroup");
+        threadGroup.setMaxPriority(7);
+
+        ThreadGroup parentThreadGroup = threadGroup;
+        String currentThreadGroupName = threadGroup.getName();
+        while ((parentThreadGroup = parentThreadGroup.getParent()) != null) {
+            System.out.printf("Thread group %s has the parent thread group %s with max priority: %d\n",
+                    currentThreadGroupName,
+                    parentThreadGroup.getName(),
+                    parentThreadGroup.getMaxPriority());
+            currentThreadGroupName = parentThreadGroup.getName();
+        }
+
+        Thread thread1 = new Thread(threadGroup, Main::runThread, "Thread 1");
+        Thread thread2 = new Thread(threadGroup, Main::runThread, "Thread 2");
+        Thread thread3 = new Thread(threadGroup, Main::runThread, "Thread 3");
+
+        thread1.setPriority(Thread.MAX_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        System.out.println("Sleeping for 3 seconds");
+        Thread.sleep(3000);
+
+        threadGroup.interrupt();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+    }
+
+    private static void runThread() {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread currentThread = Thread.currentThread();
+                System.out.printf("Thread %s with priority %d was interrupted\n", currentThread.getName(), currentThread.getPriority());
+                break;
+            }
+        }
+    }
+
+}
