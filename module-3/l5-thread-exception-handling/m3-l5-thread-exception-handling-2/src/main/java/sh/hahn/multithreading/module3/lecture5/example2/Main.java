02cd8d51a0bb6f1b0d6d78da5464acbf5a65f0a1
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-17 18:00:15 +0100

    Add default handler example to handle exceptions in threads (Module 3, lecture 5, example 2)

---
+++ module-3/l5-thread-exception-handling/m3-l5-thread-exception-handling-2/src/main/java/sh/hahn/multithreading/module3/lecture5/example2/Main.java
@@ -0,0 +1,22 @@
+package sh.hahn.multithreading.module3.lecture5.example2;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    throw new RuntimeException("Exception from thread");
+                },
+                "Exception-Thread");
+
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+            System.out.printf("Uncaught exception in thread %s: %s%n",
+                    t.getName(),
+                    e.getMessage());
+        });
+
+        thread.start();
+        thread.join();
+    }
+
+}
