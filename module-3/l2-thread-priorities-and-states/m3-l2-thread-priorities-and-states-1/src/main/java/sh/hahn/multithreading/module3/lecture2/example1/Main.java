1899de4633eaff0492017e4f6ea0675f4886012e
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-17 12:53:14 +0100

    Add Thread priority example 1

---
+++ module-3/l2-thread-priorities-and-states/m3-l2-thread-priorities-and-states-1/src/main/java/sh/hahn/multithreading/module3/lecture2/example1/Main.java
@@ -0,0 +1,32 @@
+package sh.hahn.multithreading.module3.lecture2.example1;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+
+        Thread thread1 = createThread("Thread 1", Thread.MIN_PRIORITY);
+        Thread thread2 = createThread("Thread 2", Thread.MAX_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+    }
+
+    private static Thread createThread(String name, int priority) {
+        Thread thread1 = new Thread(Main::runThread);
+        thread1.setName(name);
+        thread1.setPriority(priority);
+        return thread1;
+    }
+
+    private static void runThread() {
+        Thread currentThread = Thread.currentThread();
+        System.out.printf("Thread %s has priority %d\n", currentThread.getName(), currentThread.getPriority());
+    }
+
+
+}
