f8f1cfa229bcffedce1d1d54e11e965bcc2ef26a
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-17 10:54:15 +0100

    Add join() to wait for myThread to finish to Thread creation example #3

--- module-3/l1-thread-creation/l1-thread-creation-3/src/main/java/sh/hahn/multithreading/module3/lecture1/example3/Main.java
+++ module-3/l1-thread-creation/l1-thread-creation-3/src/main/java/sh/hahn/multithreading/module3/lecture1/example3/Main.java
@@ -2,16 +2,15 @@ package sh.hahn.multithreading.module3.lecture1.example3;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.printf("[1] Current thread: %s\n", Thread.currentThread().getName());
 
         Thread myThread = new Thread(
-                () -> {
-                    System.out.printf("[2] Current thread: %s\n", Thread.currentThread().getName());
-                }
+                () -> System.out.printf("[2] Current thread: %s\n", Thread.currentThread().getName())
         );
         myThread.setName("MyThread");
         myThread.start();
+        myThread.join();
 
         System.out.printf("[3] Current thread: %s\n", Thread.currentThread().getName());
     }
