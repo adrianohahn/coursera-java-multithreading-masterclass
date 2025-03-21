a1a33a0ddf0f0e730df7223024148bb29642b1ac
Adriano Hahn <adriano.hahn@softwareag.com>
2025-03-21 17:27:29 +0100

    Add ReadWriteLock example (Module 4, Lecture 5)

---
+++ module-4/l5-read-write-locks/m4-l5-read-write-locks/src/main/java/sh/hahn/multithreading/module4/lecture5/locks/read_write/Main.java
@@ -0,0 +1,68 @@
+package sh.hahn.multithreading.module4.lecture5.locks.read_write;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Main {
+
+    private static final Random random = new Random();
+    private static final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private static final Lock readLock = lock.readLock();
+    private static final Lock writeLock = lock.writeLock();
+    private static final List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Thread(Main::runWrite).start();
+        new Thread(Main::runRead).start();
+        new Thread(Main::runRead).start();
+        new Thread(Main::runRead).start();
+    }
+
+    private static void runWrite() {
+        while (true) {
+            sleep(10000);
+
+            writeLock.lock();
+
+            int randomInt = random.nextInt(100);
+            System.out.printf("Producing data: %d\n", randomInt);
+            list.add(randomInt);
+
+            sleep(3000);
+
+            writeLock.unlock();
+        }
+    }
+
+    private static void runRead() {
+        while (true) {
+            sleep(4000);
+
+            while (!readLock.tryLock()) {
+                System.out.println("Waiting for read lock...");
+                sleep(100);
+            }
+
+            System.out.printf("List is: %s\n", list);
+
+            readLock.unlock();
+        }
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+}
